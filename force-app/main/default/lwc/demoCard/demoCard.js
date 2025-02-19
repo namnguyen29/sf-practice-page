0299b3ab680356f8f8d98e2ceae1d17781027217
namnguyen29 <91844976+namnguyen29@users.noreply.github.com>
2025-02-19 14:00:19 +0700

    maintain dev1 cert (#2)
    Co-authored-by: Nguyen Quoc Nam <nnguyenquoc@openwt.com>

--- force-app/main/default/lwc/demoCard/demoCard.js
+++ force-app/main/default/lwc/demoCard/demoCard.js
@@ -1,5 +1,12 @@
 import { LightningElement } from "lwc";
 
 export default class DemoCard extends LightningElement {
-  name = "world";
+  mapMarkers = [
+    {
+      location: {
+        Latitude: "37.790197",
+        Longitude: "-122.396879"
+      }
+    }
+  ];
 }
