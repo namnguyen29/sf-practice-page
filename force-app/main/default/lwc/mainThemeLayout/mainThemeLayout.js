f71df5d9009ccba1ebe02d4ff637e13a44341376
namnguyen29 <91844976+namnguyen29@users.noreply.github.com>
2025-02-20 14:44:18 +0700

    add home page layout (#5)
    Co-authored-by: Nguyen Quoc Nam <nnguyenquoc@openwt.com>

--- force-app/main/default/lwc/mainThemeLayout/mainThemeLayout.js
+++ force-app/main/default/lwc/mainThemeLayout/mainThemeLayout.js
@@ -3,6 +3,6 @@ import { LightningElement } from "lwc";
 /**
  * @slot mainHeader
  * @slot mainFooter
- * @slot mainContent
  */
-export default class MainTheme extends LightningElement {}
+
+export default class MainThemeLayout extends LightningElement {}
